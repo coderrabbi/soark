0a877318ff7cdf32e278a0a520a5d01e105bfb0d
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-06 00:37:17 +0600

    all done

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -5,7 +5,7 @@ const Loader = () => {
   return ReactDOM.createPortal(
     <div className="fixed w-full h-full bg-black/30 z-10">
       <div className="fixed left-[50%] top-[50%] z-[999] -translate-x-[50%] -translate-y-[50%]">
-        <div className="line-wobble"></div>;
+        <div className="line-wobble"></div>
       </div>
     </div>,
     document.getElementById("loader")
